a158bd849309e9a348359c3df9db6039ac824ab0
martindeeks346 <martindeeks346@outlook.com>
2025-04-30 17:59:30 +0700

    Update dk.sh

--- dk.sh
+++ dk.sh
@@ -1,10 +1,24 @@
 echo Downloading
-curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa -y 
 sudo apt update
-sudo apt install playit -y
 sudo apt install firefox-trunk -y
+
+mkdir -p "$HOME/.vnc"
+sleep 1 && mkdir /home/user1/.vnc 
+sleep 1 && mkdir /home/user2/.vnc
+sleep 1 && mkdir /home/user3/.vnc
+sleep 1 && echo 71913622 | vncpasswd -f > /root/.vnc/passwd
+sleep 1 && echo 71913622 | vncpasswd -f > /home/user1/.vnc/passwd 
+sleep 1 && echo 71913622 | vncpasswd -f > /home/user2/.vnc/passwd 
+sleep 1 && echo 71913622 | vncpasswd -f > /home/user3/.vnc/passwd 
+sleep 1 && chown -R root:root /root/.vnc
+sleep 1 && chown -R user1:user1 /home/user1/.vnc
+sleep 1 && chown -R user2:user2 /home/user2/.vnc
+sleep 1 && chown -R user3:user3 /home/user3/.vnc
+sleep 1 && chmod 0600 /root/.vnc/passwd
+sleep 1 && chmod 0600 /home/user1/.vnc/passwd
+sleep 1 && chmod 0600 /home/user2/.vnc/passwd
+sleep 1 && chmod 0600 /home/user3/.vnc/passwd
 
 sleep 1 && sudo tightvncserver -geometry 1280x720 :7 > /dev/null
 sleep 1 && sudo tightvncserver -kill :7
